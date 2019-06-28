3db9058bac86aa518cb427414bd513d01c96b185
A8sprout8pig <18378338756@163.com>
2019-06-28 11:07:19 +0800

    群智提交峰

---
+++ src/main/java/com/sxftest/sxf.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: sxf
+ * Author:   1
+ * Date:     2019/6/28 11:06
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.sxftest;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 1
+ * @create 2019/6/28
+ * @since 1.0.0
+ */
+public class sxf {
+    public static void main(String[] args) {
+
+
+        System.out.println("你好啊 ，群智科技！");
+    }
+
+}
