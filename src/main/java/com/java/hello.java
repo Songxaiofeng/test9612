88cee5e438e018454093fa7846c59544d07e060e
A8sprout8pig <18378338756@163.com>
2019-06-28 10:32:32 +0800

    修改hell文件提交

---
+++ src/main/java/com/java/hello.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: hello
+ * Author:   1
+ * Date:     2019/6/28 10:31
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.java;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 1
+ * @create 2019/6/28
+ * @since 1.0.0
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world !");
+    }
+
+}
